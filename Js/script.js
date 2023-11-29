22ea79168b603b924c1afdefa23ce6c47f0fb36d
555KIV <a555kiv.master2015@yandex.ru>
2023-11-29 23:40:03 +0300

    Fix

--- Js/script.js
+++ Js/script.js
@@ -3,17 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function addTask() {
-    var taskInput = document.getElementById('new-task');
-    var taskText = taskInput.value;
+    let taskInput = document.getElementById('new-task');
+    let taskText = taskInput.value;
 
     if (taskText.trim() === '') {
         alert('Please enter a task.');
         return;
     }
 
-    var tasksList = document.getElementById('tasks');
+    let tasksList = document.getElementById('tasks');
 
-    var taskItem = document.createElement('li');
+    let taskItem = document.createElement('li');
     taskItem.className = 'todo-item';
     taskItem.innerHTML = `
                 <span>${taskText}</span>
@@ -29,27 +29,27 @@ function addTask() {
 }
 
 function removeTask(button) {
-    var taskItem = button.parentNode;
-    var tasksList = document.getElementById('tasks');
+    let taskItem = button.parentNode;
+    let tasksList = document.getElementById('tasks');
     tasksList.removeChild(taskItem);
 
     saveTasks();
 }
 
 function toggleTask(button) {
-    var taskItem = button.parentNode;
+    let taskItem = button.parentNode;
     taskItem.classList.toggle('completed');
 
     saveTasks();
 }
 
 function saveTasks() {
-    var tasksList = document.getElementById('tasks');
-    var tasks = [];
+    let tasksList = document.getElementById('tasks');
+    let tasks = [];
 
-    for (var i = 0; i < tasksList.children.length; i++) {
-        var taskText = tasksList.children[i].querySelector('span').innerText;
-        var isCompleted = tasksList.children[i].classList.contains('completed');
+    for (let i = 0; i < tasksList.children.length; i++) {
+        let taskText = tasksList.children[i].querySelector('span').innerText;
+        let isCompleted = tasksList.children[i].classList.contains('completed');
         tasks.push({ text: taskText, completed: isCompleted });
     }
 
@@ -57,14 +57,14 @@ function saveTasks() {
 }
 
 function loadTasks() {
-    var tasksList = document.getElementById('tasks');
-    var storedTasks = localStorage.getItem('tasks');
+    let tasksList = document.getElementById('tasks');
+    let storedTasks = localStorage.getItem('tasks');
 
     if (storedTasks) {
-        var tasks = JSON.parse(storedTasks);
+        let tasks = JSON.parse(storedTasks);
 
-        for (var i = 0; i < tasks.length; i++) {
-            var taskItem = document.createElement('li');
+        for (let i = 0; i < tasks.length; i++) {
+            let taskItem = document.createElement('li');
             taskItem.className = 'todo-item' + (tasks[i].completed ? ' completed' : '');
             taskItem.innerHTML = `
                         <span>${tasks[i].text}</span>
